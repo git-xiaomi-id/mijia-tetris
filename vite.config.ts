033e22586adb8457636589ae27284c6b640dd3f5
git-xiaomi-id <id.xiaomicomm@gmail.com>
2025-05-31 09:40:52 +0700

    add config image

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import viteImageMin from "vite-plugin-imagemin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteImageMin({
+      gifsicle: { optimizationLevel: 7 },
+      mozjpeg: { quality: 80 },
+      optipng: { optimizationLevel: 7 },
+      pngquant: { quality: [0.8, 0.9] },
+      svgo: {
+        plugins: [
+          { name: "removeViewBox" },
+          { name: "removeEmptyAttrs", active: false },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
