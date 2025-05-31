8bdbc9d89f82957e5957e67b3aeab6f6823048fc
git-xiaomi-id <id.xiaomicomm@gmail.com>
2025-05-31 15:50:22 +0700

    replace @ string

--- src/lib/supabase-client.ts
+++ src/lib/supabase-client.ts
@@ -10,26 +10,28 @@ export async function getUserByToken(token: string) {
 }
 
 export async function checkAlreadyLoggedIn(username_ig: string, token: string) {
+  const username = username_ig.replace("@", "");
   if (!token) return null;
   const res = await clientSupabase
     .from("user")
     .select()
-    .eq("username_ig", username_ig)
+    .eq("username_ig", username)
     .single();
-  if (res.error) return await createUser(username_ig, token);
+  if (res.error) return await createUser(username, token);
   else if (res.data && res.data.token)
     return { data: null, error: { message: "already-logged-in" } };
   return res;
 }
 
 export async function createUser(username_ig: string, token: string) {
+  const username = username_ig.replace("@", "");
   const updated_at = new Date().toISOString();
   return await clientSupabase
     .from("user")
     .upsert(
       {
         token,
-        username_ig,
+        username_ig: username,
         updated_at,
       },
       { onConflict: "username_ig" }
@@ -39,30 +41,33 @@ export async function createUser(username_ig: string, token: string) {
 }
 
 export async function removeUserToken(token: string, username_ig: string) {
+  const username = username_ig.replace("@", "");
   if (!token) return null;
   return await clientSupabase
     .from("user")
     .update({ token: "" })
     .eq("token", token)
-    .eq("username_ig", username_ig!)
+    .eq("username_ig", username!)
     .select()
     .maybeSingle();
 }
 
 export async function getIdByUsername(username_ig: string) {
+  const username = username_ig.replace("@", "");
   if (!username_ig) return null;
   return await clientSupabase
     .from("user")
     .select("id")
-    .eq("username_ig", username_ig);
+    .eq("username_ig", username);
 }
 
 export async function removeTokenFromUser(username_ig: string) {
+  const username = username_ig.replace("@", "");
   if (!username_ig) return null;
   const res = await clientSupabase
     .from("user")
     .update({ token: null })
-    .eq("username_ig", username_ig.replace("@", ""))
+    .eq("username_ig", username)
     .select()
     .maybeSingle();
   return res;
@@ -73,10 +78,11 @@ export async function checkValidUser(
   token: string,
   id: string
 ) {
+  const username = username_ig.replace("@", "");
   return await clientSupabase
     .from("user")
     .select()
-    .eq("username_ig", username_ig)
+    .eq("username_ig", username)
     .eq("token", token)
     .eq("id", id)
     .maybeSingle();
