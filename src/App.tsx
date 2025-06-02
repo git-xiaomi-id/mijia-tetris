a6f890ed9b1fe88c8aacf55936ad7e0b8390be13
git-xiaomi-id <id.xiaomicomm@gmail.com>
2025-06-02 10:28:13 +0700

    adjust toast

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,15 @@ export default function App() {
       <ToasterSonner
         toastOptions={{
           classNames: {
-            toast: " p-4 rounded-lg flex items-start gap-4  shadow-lg",
+            toast:
+              " py-2 px-3 rounded-[12px] flex items-start gap-4 border  shadow-lg",
             error:
-              " bg-gradient-to-b from-[#FFFFFF] to-[#EFEFEF] border-[#d04141] text-[#232322]",
+              " bg-gradient-to-b from-[#FFFFFF] to-[#EFEFEF] border-[#d04141] text-[#232322] shadow-[0px_4px_12px_0px_#C4090966] font-[450]",
             success:
-              "bg-gradient-to-b from-[#FFFFFF] to-[#EFEFEF] border-[#2e7d32] text-[#232322]",
+              "bg-gradient-to-b from-[#FFFFFF] to-[#EFEFEF] border-[#2e7d32] text-[#232322] shadow-[0px_4px_12px_0px_#2e7d3266] font-[450]",
             warning:
-              "bg-gradient-to-b from-[#FFFFFF] to-[#EFEFEF] border-[#ed6c02] text-[#232322]",
-            info: "bg-gradient-to-b from-[#FFFFFF] to-[#EFEFEF] border-[#0288d1] text-[#232322]",
+              "bg-gradient-to-b from-[#FFFFFF] to-[#EFEFEF] border-[#ed6c02] text-[#232322] shadow-[0px_4px_12px_0px_#ed6c0266] font-[450]",
+            info: "bg-gradient-to-b from-[#FFFFFF] to-[#EFEFEF] border-[#0288d1] text-[#232322] shadow-[0px_4px_12px_0px_#0288d166] font-[450]",
             content: "text-sm tracking-wide",
             title: "font-semibold",
           },
