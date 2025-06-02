c607b073088ab735550fbc76aac0cf280f4e80ea
git-xiaomi-id <id.xiaomicomm@gmail.com>
2025-06-02 21:32:34 +0700

    add game interact

--- src/components/app/game-screen/game-interact.tsx
+++ src/components/app/game-screen/game-interact.tsx
@@ -5,29 +5,67 @@ export default function GameInteract() {
   const asset = assets[assets.length - 1];
 
   return (
-    screenSteps.findIndex((n) => n === screenStep) >= 2 && (
+    screenSteps.findIndex((n) => n === screenStep) >= 3 && (
       <div className="absolute left-0 top-0 size-full">
         <div className="size-full relative border border-red-500 border-dashed">
           <img
             key={asset.key}
             alt={asset?.key || ""}
             src={asset?.src || ""}
-            className="h-full object-contain block mx-auto"
+            className={[
+              "h-full object-contain block mx-auto transition-all",
+              screenStep === "game" ? "scale-110" : "",
+            ].join(" ")}
           />
           {screenStep === "game" && (
             <>
-              <div className="gs-mask-door-dashed door-left " />
-              <div className="gs-mask-door-dashed door-middle  " />
-              <div className="gs-mask-door-dashed door-right  " />
+              <button
+                type="button"
+                className="gs-mask-door-dashed door-left "
+              />
+              <button
+                type="button"
+                className="gs-mask-door-dashed door-middle  "
+              />
+              <button
+                type="button"
+                className="gs-mask-door-dashed door-middle2  "
+              />
+              <button
+                type="button"
+                className="gs-mask-door-dashed door-middle3  "
+              />
+              <button
+                type="button"
+                className="gs-mask-door-dashed door-right  "
+              />
 
-              <div className="gs-mask-door-dashed-middle door-left  " />
-              <div className="gs-mask-door-dashed-middle door-right  " />
+              <button
+                type="button"
+                className="gs-mask-door-dashed-middle door-left  "
+              />
+              <button
+                type="button"
+                className="gs-mask-door-dashed-middle door-right  "
+              />
 
-              <div className="gs-mask-door-dashed-bottom left" />
-              <div className="gs-mask-door-dashed-bottom right" />
+              <button
+                type="button"
+                className="gs-mask-door-dashed-bottom left"
+              />
+              <button
+                type="button"
+                className="gs-mask-door-dashed-bottom right"
+              />
 
-              <div className="gs-mask-door-dashed-freezerbottom left" />
-              <div className="gs-mask-door-dashed-freezerbottom right" />
+              <button
+                type="button"
+                className="gs-mask-door-dashed-freezerbottom left"
+              />
+              <button
+                type="button"
+                className="gs-mask-door-dashed-freezerbottom right"
+              />
 
               {/* <div className="gs-mask-door-freezerbottom sec2 left" />
               <div className="gs-mask-door-freezerbottom sec2 right" /> */}
