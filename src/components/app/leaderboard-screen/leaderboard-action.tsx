5fe06a74c4b38e5e8f8a2ddb968e6952d7c68c6e
faizmrtdha <faizmrtdha99@gmail.com>
2025-06-06 01:52:05 +0700

    chore: add validation live chance in leaderboard page

--- src/components/app/leaderboard-screen/leaderboard-action.tsx
+++ src/components/app/leaderboard-screen/leaderboard-action.tsx
@@ -4,17 +4,28 @@ import { RotateCw } from "lucide-react";
 import { useNavigate } from "react-router";
 import AppButton from "../button";
 import FinishHome from "../finish-screen/icons/finish-home";
+import AppModal from "../welcome-screen/modal";
+import { useState } from "react";
 
 export default function LeaderboardAction() {
   const navigate = useNavigate();
   const { runScenario, doResetGame } = useGameProvider();
-  const { setScreen } = useAppProvider();
+  const { setScreen, gamesCount } = useAppProvider();
+  const [modal, setModal] = useState<boolean>(false);
 
   function doRestartGame() {
     doResetGame();
     runScenario();
     setScreen("game");
     navigate("/");
+  }
+
+  function checkChances() {
+    if (gamesCount >= 3) {
+      setModal(true);
+    } else {
+      doRestartGame();
+    }
   }
 
   function goHome() {
@@ -25,7 +36,7 @@ export default function LeaderboardAction() {
 
   return (
     <div className="ls-action">
-      <AppButton variant="orange" className="flex-1" onClick={doRestartGame}>
+      <AppButton variant="orange" className="flex-1" onClick={checkChances}>
         <div className="flex items-center gap-2.5 w-full [&_svg]:size-6">
           <RotateCw className="flex-shrink-0 -rotate-90" />
           <div className="text-center text-base">Main lagi</div>
@@ -37,6 +48,17 @@ export default function LeaderboardAction() {
           <div className="text-center text-base">Ke Home</div>
         </div>
       </AppButton>
+      {modal && (
+        <AppModal
+          open={modal}
+          title="Kesempatan main habis"
+          description="Kamu sudah menghabiskan semua kesempatan main hari ini. Kembali lagi besok untuk mendapatkan 3 kesempatan main tambahan."
+          image="/mi-bunny/mi-bunny-cry.webp"
+          animationImage="animate-headshaking"
+          textConfirm="Oke, mengerti"
+          onOpenChange={setModal}
+        />
+      )}
     </div>
   );
 }
